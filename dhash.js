68bcafdeeb2ef278a81e84a537af4289606ef56e
Connor Atherton <connor@bitnami.com>
2014-11-03 22:59:40 -0800

    Initial implementation of dhash

---
+++ dhash.js
@@ -0,0 +1,31 @@
+function DHash() {
+  if (!(this instanceof DHash))
+    return new DHash();
+
+  this.store = {};
+}
+
+DHash.prototype.get = function(key, setter) {
+  if (this.store[key]) {
+    var value = this.store[key];
+    delete this.store[key];
+    return value;
+  }
+
+  return setter ? this.store[key] = setter() : undefined;
+};
+
+DHash.prototype.set = function(key, value) {
+  if (key && value)
+     return this.store[key] = value;
+};
+
+DHash.prototype.flush = function() {
+  delete this.store;
+  return this.store = {};
+};
+
+module.exports = DHash;
+
+
+
