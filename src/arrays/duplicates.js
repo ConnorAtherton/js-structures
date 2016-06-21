230056b413147b58ea2305f85e5c5943cd7c9582
Connor Atherton <connor@bitnami.com>
2016-06-21 00:11:27 -0700

    Sum and duplicates functions

---
+++ src/arrays/duplicates.js
@@ -0,0 +1,89 @@
+//
+// Time: O(n**2)
+//
+const brute = (arr) => {
+  let duplicates = []
+
+  for (const outer of arr) {
+    for (const inner of arr) {
+      if (outer === inner) {
+        duplicates.push(outer)
+        continue
+      }
+    }
+  }
+
+  return duplicates
+}
+
+//
+// Time: O(n)
+// Space: O(n)
+//
+const hash = (arr) => {
+  let values = {}
+  let duplicates = []
+
+  for (const val of arr) {
+    if (values[val] !== undefined) {
+      values[val] = values[val]++
+    } else {
+      values[val] = 0
+    }
+  }
+
+  Object.keys(values).forEach(key => {
+    if (values[key] > 0) duplicates.push(values[key])
+  })
+
+  return duplicates
+}
+
+//
+// If the input is small enough and the possible values are constrained enough
+// then we can allocate a zeroed out array and mark each index value as 1 when we
+// encounter it. If we see an index that is greater than 0, we can store it an
+// auxiliiary storage and return.
+//
+const array = (arr) => {
+
+}
+
+//
+// ...cheeky
+//
+const set = (arr) => {
+  return Array.from(new Set(arr))
+}
+
+//
+// eg: [1, 2, 3, 4, 5, 6
+//
+const singleMissingInKnownRange = (arr) => {
+
+}
+
+//
+// mutable:
+//   Switch the values at indicies.
+//
+const constantSpaceMutable = (arr) => {
+
+}
+
+//
+// immutable:
+//   Treat the values as an ordered graph and find the cycle, since there is guaranteed to
+//   be a cycle if we have duplicates.
+//
+const constantSpaceImmutable = (arr) => {
+}
+
+export {
+  brute,
+  hash,
+  array,
+  set,
+  singleMissingInKnownRange,
+  constantSpace
+}
---
+++ src/arrays/sum.js
@@ -0,0 +1,61 @@
+//
+// Given an array of numbers, return 2 numbers that form to a given sum, or null otherwise.
+//
+
+//
+//
+//
+const brute = (arr) => {
+
+}
+
+//
+// Loop through and store each entry into hash table
+//
+// Space: O(n)
+// Time: O(n)
+//
+const hash = (arr) => {
+
+}
+
+//
+// Hash, but assume duplicates are possible. We can count the duplcates as the hash
+// key here.
+//
+// Space: O(n)
+// Time: O(n)
+//
+//
+const hashWithDuplicates = (arr) => {
+
+}
+
+//
+// For large arrays, the above methods might be too costly as we have to every value
+// twice: once in the original array, and another in a data structure used to count the
+// occurence.
+//
+// Quicksort is an in-place sort, and usually not stable.
+//
+// Time: O(1)
+// Storage: O(n)
+//
+//
+const inPlace = (arr) => {
+
+}
+
+// EXTENSION
+//
+// Given an array of integers check whether there are 3 numbers that sum up to a given number
+//
+const threeNumbers = (arr) => {
+  // Iterate through the array and use the approach for finding two numbers outlined above
+}
+
+export {
+  hash,
+  hashWithDuplicates,
+  inPlace
+}
